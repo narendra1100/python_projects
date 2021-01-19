0cb34bccd8e16740e743d87451e1690a610ef9cc
narendra1100 <77606205+narendra1100@users.noreply.github.com>
2021-01-18 19:09:17 -0800

    Add files via upload

---
+++ bank trasfer.py
@@ -0,0 +1,35 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======================== RESTART: E:/bank project.py ========================
+************************************WELCOME TO SEVEN HILLS BANK*************************************
+__________PLEACE INSERT YOUR 'ATM' CORD___________
+pleace enter you pasword here
+2
+3
+4
+5
+ENQURY
+WITH DRAW
+TRANSFER
+DEPOSIT
+note :
+                            either you want to with draw
+                            or you want to transfer
+                            or you want to deposit
+                            you must see the bank balence
+if you want to see balence you must enter like this BALENCE :balence
+your balence is = 56305
+if do you want to "with draw"or "transfer" or "deposit" .pleace enter (yes /no):yes
+if do you want to
+                                                    ----------->withdraw type like this:withdraw
+                                                    ----------->transfer  type like this :transfer
+                                                    ----------->deposit  type like this  :deposit   
+type here:transfer
+pleace enter your 12 digit account no:324567654321
+pleace enter your 10 digit number:7755443322
+pleace enter your 12 digits of tracsfer account number:234543123543
+enter your trasfer amout :500
+your remaing balence is: 55805
+THANK YOU
+>>> 
