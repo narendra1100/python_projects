3c29965272560699ef962ba5ec17053dbdff7a87
narendra1100 <77606205+narendra1100@users.noreply.github.com>
2021-01-18 19:10:11 -0800

    Add files via upload
    c pattren

---
+++ c patren.py
@@ -0,0 +1,10 @@
+##for row in range(1,5):
+##    for col in range(1,5):
+##        if 1%col==0 and 2%col ==0 and3%col==0:
+##            print("*",end="")
+##    print()
+for  n in range (1,5):
+    print("*",end="")
+for k in range(1,3):
+    if 2%k==0 and:
+        print("*")
